a8ab74c2144231d1d63ab1c40aeafbafa7d43ea5
Adam <adamwalkerlondon@gmail.com>
2021-08-30 14:01:31 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# React Hangman Game
+
---
+++ src/AlphaButtons.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+class AlphaButtons  extends React.Component {
+    render() {
+
+       
+        return (
+            <div className="AlphaButtons ">
+              <h1 >AlphaButtons </h1>
+            </div>
+        )
+    }
+}
+
+export default AlphaButtons 
